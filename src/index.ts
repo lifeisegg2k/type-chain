fb3dfd7fb35ae6d26bf672262ab944fe805d5006
lifeisegg <lifeisegg@gmail.com>
2021-12-10 17:29:43 +0900

    3th

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+import { Agent } from "http";
+
+interface Human {
+    name: string,
+    age: number,
+    gender: string
 }
 
-console.log(sayHi("test", 22, "male"));
+const person = {
+    name: "test name",
+    age: 18,
+    gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+}
+
+console.log(sayHi(person));
 
 export {};
