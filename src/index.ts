86d4dc50552dee13c25d9a037f8a180335cbfabc
lifeisegg <lifeisegg@gmail.com>
2021-12-10 17:36:34 +0900

    4th

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import { Agent } from "http";
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
 
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
+
+const lynn = new Human("Lynn", 18, "female");
 
 const person = {
     name: "test name",
@@ -16,6 +22,6 @@ const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
