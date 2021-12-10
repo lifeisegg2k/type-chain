85ce7b30d2602f4bacfe8df30206c603b5cd59f9
lifeisegg <lifeisegg@gmail.com>
2021-12-10 18:04:24 +0900

    7th

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,28 @@ import * as CryptoJS from "crypto-js";
 class Block {
   public index: number;
   public hash: string;
-  public previousHash: string;
+  public previosHash: string;
   public data: string;
   public timestamp: number;
 
   static calculateBlockHash = (
     index: number, 
-    previousHash: string, 
+    previosHash: string, 
     timesteamp: number, 
     data: string
   ): string => 
-    CryptoJS.SHA256(index + previousHash + timesteamp + data).toString();
+    CryptoJS.SHA256(index + previosHash + timesteamp + data).toString();
 
   constructor(
     index: number,
     hash: string,
-    previousHash: string,
+    previosHash: string,
     data: string,
     timestamp: number
   ){
     this.index = index;
     this.hash = hash;
-    this.previousHash = previousHash;
+    this.previosHash = previosHash;
     this.data = data;
     this.timestamp = timestamp;
   }
@@ -39,5 +39,27 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length -1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex, 
+    previosBlock.hash, 
+    newTimestamp, 
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("Hello"), createNewBlock("bye bye"));
 
 export {};
